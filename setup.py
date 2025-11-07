bdb320d7c3e6d35b3394a8eabe659f718e76a6ac
bjblank2 <bjblank2@illinois.edu>
2025-11-07 10:39:37 -0600

    Update maintainer email in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ros',
-    maintainer_email='isaac.j.blankenau@usace.army.mil',
+    maintainer_email='brianjblank7@gmail.com',
     description='TODO: Package description',
     license='Apache-2.0',
     extras_require={
